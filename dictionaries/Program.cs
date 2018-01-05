a219bf4c0da92dfc76c5935bed3b24923dd4ad75
Gward2489 <gww8804@gmail.com>
2018-01-05 14:35:17 -0600

    first commit

---
+++ dictionaries/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> stocks = new Dictionary<string, string>();
+
+            stocks.Add("GM", "General Motors");
+            stocks.Add("CAT", "Caterpillar");
+            stocks.Add("IGC", "India Globalization Capital");
+            stocks.Add("RCK", "Rock Masters");
+
+            List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
+
+            purchases.Add((ticker: "GM", shares: 150, price: 23.21));
+            purchases.Add((ticker: "GM", shares: 150, price: 30.21));
+            purchases.Add((ticker: "CAT", shares: 200, price: 17.84));
+            purchases.Add((ticker: "IGC", shares: 45, price: 1.74));
+            purchases.Add((ticker: "IGC", shares: 45, price: 2.01));
+            purchases.Add((ticker: "RCK", shares: 95, price: 6.66));
+
+            Dictionary<string, double> purchaseInfo = new Dictionary<string, double>();
+
+            foreach ((string, int, double) purchase in purchases){
+                double totalValue = purchase.Item3 * purchase.Item2;
+                foreach (KeyValuePair<string, string> stockNames in stocks){
+                    if (purchase.Item1 == stockNames.Key){
+                        if(purchaseInfo.ContainsKey(stockNames.Value) == false){
+                        purchaseInfo.Add(stockNames.Value, totalValue);
+                        } else {
+                            purchaseInfo[stockNames.Value] += totalValue;
+                        }
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, double> purchasedStock in purchaseInfo){
+                Console.WriteLine($"{purchasedStock.Key}: ${purchasedStock.Value}");
+            }
+        }
+    }
+}
---
+++ tuples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product,  int quantity, double amount)> transactions = new List<(string, int, double)>();
+            
+            (string name, int quantity, double price) productOne = ("Yo-yo", 1, 15.00);
+            (string name, int quantity, double price) productTwo = ("Skateboard", 1, 70.50);
+            (string name, int quantity, double price) productThree = ("Bicycle", 1, 340.92);
+            (string name, int quantity, double price) productFour = ("Red Hat", 2, 24.99);
+            (string name, int quantity, double price) productFive = ("Baseball Bat", 1, 38.87);
+
+            transactions.Add(productOne);
+            transactions.Add(productTwo);
+            transactions.Add(productThree);
+            transactions.Add(productFour);
+            transactions.Add(productFive);
+
+            int totalSalesQuantity = 0;
+            double totalSalesRev = 0;
+
+            foreach ((string, int, double) shopItem in transactions){
+                totalSalesQuantity += shopItem.Item2;
+                totalSalesRev += shopItem.Item3;
+            };
+
+            Console.WriteLine($"Total items sold today: {totalSalesQuantity}");
+            Console.WriteLine($"Total revenue for the day: {totalSalesRev}");
+        
+        }
+    }
+}
