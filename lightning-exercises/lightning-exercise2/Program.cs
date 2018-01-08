b2f12500948962a3352cb92084247184b97995a9
Gward2489 <gww8804@gmail.com>
2018-01-08 14:45:05 -0600

    finished part 2

--- lightning-exercises/lightning-exercise2/Program.cs
+++ lightning-exercises/lightning-exercise2/Program.cs
@@ -20,6 +20,28 @@ namespace lightning_exercise2
 
             List<Dictionary<string, Dictionary<int, string>>> thirdDictionary = new List<Dictionary<string, Dictionary<int, string>>>();
 
+
+
+            List<int> numbers = new List<int>(){ 23, 45, 36, 39, 102, 493, 474, 34, 11, 35, 99 };
+
+            // Using Linq...
+            // 1. Find the sum of numbers
+            int sum = numbers.Sum();
+
+
+            // 2. Find the average of the numbers
+            double average = numbers.Average();
+
+            // 3. Create a new List of numbers greater than 30
+
+            List<int> newNumbers = new List<int>();
+
+            foreach(int numb in numbers){
+                if (numb > 30){
+                    newNumbers.Add(numb);
+                }
+            }
+
         }
     }
 }
