c660c837461141ec8a26acd272a31636a77631e7
Gward2489 <gww8804@gmail.com>
2018-01-08 14:36:42 -0600

    adding 2nd lightning exercise

---
+++ lightning-exercises/lightning-exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lightning_exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Dictionary with a key of type string and value type of double
+
+            Dictionary<string, double> firstDictionary = new Dictionary<string, double>();
+
+            // 2. Dictionary with key of type string and value of type List<int[]>
+
+            Dictionary<string, List<int[]>> secondDictionary = new Dictionary<string, List<int[]>>();
+
+            // 3. List of dictionaries with a key of type string and a value of type dictionary with a key of type int and value of type string
+
+            List<Dictionary<string, Dictionary<int, string>>> thirdDictionary = new List<Dictionary<string, Dictionary<int, string>>>();
+
+        }
+    }
+}
